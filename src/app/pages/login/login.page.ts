a92a5b5d98fb205b31706feb0ea27d8486c78c4b
Awab Abdoun <awabcodes@gmail.com>
2019-09-26 10:59:04 +0200

    login and signup page improvement

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, ToastController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { LoginService } from 'src/app/services/login/login.service';
+import { CustomAlertService } from 'src/app/services/custom-alert.service';
 
 @Component({
   selector: 'app-login',
@@ -9,28 +10,20 @@ import { LoginService } from 'src/app/services/login/login.service';
   styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-  // The account fields for the login form.
   account: { username: string; password: string; rememberMe: boolean } = {
     username: '',
     password: '',
     rememberMe: false
   };
 
-  // Our translated text strings
-  private loginErrorString: string;
-
   constructor(
     public translateService: TranslateService,
     public loginService: LoginService,
-    public toastController: ToastController,
+    public customAlertService: CustomAlertService,
     public navController: NavController
   ) {}
 
-  ngOnInit() {
-    this.translateService.get('LOGIN_ERROR').subscribe(value => {
-      this.loginErrorString = value;
-    });
-  }
+  ngOnInit() {}
 
   doLogin() {
     this.loginService.login(this.account).then(
@@ -38,14 +31,8 @@ export class LoginPage implements OnInit {
         this.navController.navigateRoot('/tabs');
       },
       async err => {
-        // Unable to log in
         this.account.password = '';
-        const toast = await this.toastController.create({
-          message: this.loginErrorString,
-          duration: 3000,
-          position: 'top'
-        });
-        toast.present();
+        this.customAlertService.showToast('LOGIN_ERROR');
       }
     );
   }
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, ToastController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { UserService } from 'src/app/services/user/user.service';
+import { CustomAlertService } from 'src/app/services/custom-alert.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +10,6 @@ import { UserService } from 'src/app/services/user/user.service';
   styleUrls: ['./signup.page.scss']
 })
 export class SignupPage implements OnInit {
-  // The account fields for the signup form
   account: {
     login: string;
     email: string;
@@ -26,25 +26,13 @@ export class SignupPage implements OnInit {
     langKey: 'en'
   };
 
-  // Our translated text strings
-  private signupErrorString: string;
-  private signupSuccessString: string;
-  private existingUserError: string;
-  private invalidPasswordError: string;
 
   constructor(
     public navController: NavController,
     public userService: UserService,
-    public toastController: ToastController,
+    public customAlertService: CustomAlertService,
     public translateService: TranslateService
-  ) {
-    this.translateService.get(['SIGNUP_ERROR', 'SIGNUP_SUCCESS', 'EXISTING_USER_ERROR', 'INVALID_PASSWORD_ERROR']).subscribe(values => {
-      this.signupErrorString = values.SIGNUP_ERROR;
-      this.signupSuccessString = values.SIGNUP_SUCCESS;
-      this.existingUserError = values.EXISTING_USER_ERROR;
-      this.invalidPasswordError = values.INVALID_PASSWORD_ERROR;
-    });
-  }
+  ) {}
 
   ngOnInit() {}
 
@@ -54,33 +42,23 @@ export class SignupPage implements OnInit {
     // Attempt to login in through our User service
     this.userService.signup(this.account).subscribe(
       async () => {
-        const toast = await this.toastController.create({
-          message: this.signupSuccessString,
-          duration: 3000,
-          position: 'top'
-        });
-        toast.present();
+        this.customAlertService.showToast('SIGNUP_SUCCESS');
       },
       async response => {
         // Unable to sign up
         const error = JSON.parse(response.error);
-        let displayError = this.signupErrorString;
+        let displayError = 'SIGNUP_ERROR';
         if (response.status === 400 && error.type.includes('already-used')) {
-          displayError = this.existingUserError;
+          displayError = 'EXISTING_USER_ERROR';
         } else if (
           response.status === 400 &&
           error.message === 'error.validation' &&
           error.fieldErrors[0].field === 'password' &&
           error.fieldErrors[0].message === 'Size'
         ) {
-          displayError = this.invalidPasswordError;
+          displayError = 'INVALID_PASSWORD_ERROR';
         }
-        const toast = await this.toastController.create({
-          message: displayError,
-          duration: 3000,
-          position: 'middle'
-        });
-        toast.present();
+        this.customAlertService.showToast(displayError);
       }
     );
   }
